033746607a1fdb4093f87470936928e7a8618881
frousseu <francois.rousseu@usherbrooke.ca>
2024-03-17 23:13:18 -0400

    some new functions and remove init commits

--- functions.R
+++ functions.R
@@ -23,6 +23,8 @@ latest_species_commits <- function(n = 100, species = TRUE){
   })
   x <- do.call("rbind", l)
   x <- x[grep("_", x$file),]
+  rem <- c("0fe221835f4c22857bb9443434bdaedef7fb6584", "3cda8bf851afdde774cea6b32ae8d0f07a262953")
+  x <- x[!(x$sha %in% rem), ] # remove .md inits
   if(species){
     unique(x$file)
   }else{
@@ -84,10 +86,11 @@ species_paths <- function(){
   x <- x$tree$path
   x[grep("_",x)]
 }
+#x <- species_paths()
 
 
 make_species_files <- function(){
-  folders <- unique(d[ , c("family", "genus", "sp")]) |>
+  folders <- unique(d[ , c("family", "genus", "species")]) |>
     apply(1, paste, collapse = "/") |>
     gsub(" ", "_", x = _) |>
     paste0(".md") |>
@@ -132,8 +135,54 @@ list_contributions <- function(x){
   data.frame(species = names(l), contribution = unname(unlist(l)))
 }
 
+# returns a vector of inat ids
+translate2inat <- function(sp){ # make sure order returned corresponds to sp order
+  lsp<-split(sp,ceiling(seq_along(sp)/200))
+  unlist(lapply(lsp,function(i){
+    api<-paste0("http://resolver.globalnames.org/name_resolvers.json?names=",paste(gsub(" ","+",i),collapse="|"),"&data_source_ids=147|180")
+    req<-GET(api)
+    json<-content(req,as="text")
+    x<-fromJSON(json)$data$results
+    id<-sapply(x,function(j){
+      ma<-match("iNaturalist Taxonomy",j$data_source_title)
+      if(is.na(ma)){
+        NA
+      }else{
+        j$taxon_id[ma]
+      }
+    })
+    ifelse(is.na(id), NA, paste0("https://www.inaturalist.org/taxa/",id))
+  }))
+}
 
 
+get_random_photos<-function(id,license=c("cc0","cc-by","cc-by-nc"),iders=NULL,place=TRUE){
+  cc<-paste(license,collapse=",")
+  #x<-fromJSON(paste0("https://api.inaturalist.org/v1/observations?photo_license=",cc,"&taxon_id=",id,"&quality_grade=research&ident_user_id=",iders,"&order=desc&order_by=created_at"))
+  api<-paste0("https://api.inaturalist.org/v1/observations?photo_license=",cc,"&taxon_id=",id,if(is.null(place)){""}else{"&place_id=13336"},if(is.null(iders)){""}else{paste0("&ident_user_id=",iders)},"&order=desc&order_by=created_at&per_page=200")
+  x<-fromJSON(api)#$to
+  if(x$total_results==0){
+    return(NULL)
+  }else{
+    x<-x$results
+  }
+  users<-cbind(place_guess=x$place_guess,x$user[,c("login","name")])
+  pics<-do.call("rbind",lapply(seq_along(x$observation_photos),function(i){
+    res1<-x$observation_photos[[i]]$photo[,c("url","license_code","attribution")]
+    res2<-x$observation_photos[[i]]$photo$original_dimensions[,c("width","height")]
+    res<-cbind(res1,res2)
+    #res<-res[which(res$width>205 & res$height>205),]
+    #if(nrow(res)>0){
+    res<-res[1,] # keep first one
+    #}
+    cbind(idobs=as.integer(x$id[i]),res,users[rep(i,nrow(res)),])
+  }))
+  showbobs<-paste0("https://www.inaturalist.org/observations/?id=",paste0(pics$idobs,collapse=","),"&place_id=any")
+  #ids<-x$identifications[[1]][,c("taxon_id","current","user")]
+  pics$url<-gsub("/square","/medium",pics$url)
+  pics<-pics[which(pics$width>205 & pics$height>205),]
+  pics
+}
 
 
 
